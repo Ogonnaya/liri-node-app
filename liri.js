525b6ca6a44a46580ae6f772651c49250df5f4da
Ogonnaya <hello@ogonnaya.com>
2019-11-17 11:57:25 -0600

    updated functions for user commands in liri.js

--- liri.js
+++ liri.js
@@ -1,7 +1,52 @@
-js require("dotenv").config();
+// Linking dotenv file
+require("dotenv").config();
 
-js
-  var keys = require("./keys.js");
+var fs = require("fs");
+var axios = require("axios)");
 
-  js
-  var spotify = new Spotify(keys.spotify);
+var keys = require("./keys.js");
+
+// Linking spotify keys and api
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
+
+// Setting index for user inputs
+var userInput = process.argv[2];
+var userQuestion = process.argv[3];
+
+// Setting functions for user commands
+function userCommand(userInput, userQuestion) {
+  switch (userInput) {
+    case "concert-this":
+      concertThis();
+      break;
+    case "spotify-this-song":
+      spotifyThisSong();
+      break;
+    case "movie-this":
+      movieThis();
+      break;
+    case "do-what-it-says":
+      doWhatItSays();
+      break;
+    default:
+      console.log("I don't understand");
+      break;
+  }
+}
+
+function concertThis() {
+
+};
+
+function spotifyThisSong() {
+
+};
+
+function movieThis() {
+
+};
+
+function doWhatItSays() {
+
+};
